ef6ecafd254a273a48b8f1c699bac97d3b10c129
Marc Poulhiès <dkm@kataplop.net>
2021-06-23 16:24:15 +0200

    Add dummy author info in case none is configured (#36)
    This is currently needed when the script is executed in a virgin environment as
    git needs this to create the "initial commit" in the
    build_sysroot/prepare_sysroot_src.sh script.

--- build_sysroot/prepare_sysroot_src.sh
+++ build_sysroot/prepare_sysroot_src.sh
@@ -20,6 +20,13 @@ git init
 echo "[GIT] add"
 git add .
 echo "[GIT] commit"
+
+# This is needed on virgin system where nothing is configured.
+# git really needs something here, or it will fail.
+# Even using --author is not enough.
+git config user.email || git config user.email "none@example.com"
+git config user.name || git config user.name "None"
+
 git commit -m "Initial commit" -q
 for file in $(ls ../../patches/ | grep -v patcha); do
 echo "[GIT] apply" $file
