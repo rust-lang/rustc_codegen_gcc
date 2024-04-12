37798aeacdf07bd02ca1838e60c0ab00b9edc459
g4titan <samylake@outlook.com>
2024-04-12 18:16:35 +0100

    # This is a combination of 6 commits. # This is the 1st commit message:
    more explanatory build system commands
    
    # This is the commit message #2:
    
    A better description for the commands in the usage of the build system script
    
    # This is the commit message #3:
    
    better description for commands in the usage of the build system script
    
    # This is the commit message #4:
    
    update
    
    # This is the commit message #5:
    
    Update build_system/src/main.rs
    
    Co-authored-by: antoyo <antoyo@users.noreply.github.com>
    # This is the commit message #6:
    
    Update build_system/src/main.rs
    
    Co-authored-by: antoyo <antoyo@users.noreply.github.com>

--- build_system/src/main.rs
+++ build_system/src/main.rs
@@ -26,16 +26,31 @@ macro_rules! arg_error {
 fn usage() {
     println!(
         "\
-Available commands for build_system:
+rustc_codegen_gcc build system
 
-    cargo     : Run cargo command
-    clean     : Run clean command
-    prepare   : Run prepare command
-    build     : Run build command
-    test      : Run test command
-    info      : Run info command
-    clone-gcc : Run clone-gcc command
-    --help    : Show this message"
+Usage: build_system [command] [options]
+
+    Commands:
+
+        cargo     : Executes a cargo command. Use 'cargo --help' for a list of cargo commands.
+        clean     : Cleans the build directory, removing all compiled files and artifacts.
+        prepare   : Prepares the environment for building, including fetching dependencies and setting up configurations.
+        build     : Compiles the project. Use 'build --help' for build options.
+        test      : Runs tests for the project. Use 'test --help' for test options.
+        info      : Displays information about the build environment and project configuration.
+        clone-gcc : Clones the GCC compiler from a specified source. Use 'clone-gcc --help' for options.
+        --help    : Shows a help message.
+
+Options:
+        -h, --help    : Displays this help message.
+
+        ./y.sh build
+        ./y.sh test --release
+        ./y.sh info
+        ./y.sh clone-gcc --source=https://example.com/gcc.tar.gz
+
+        Note: Replace 'build_system' with the actual name of your build script executable if different.
+        "
     );
 }
 
